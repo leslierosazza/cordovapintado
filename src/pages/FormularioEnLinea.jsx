1d01d057e21727048746aa3512f9d5c7e041eee2
Leslie Velasquez Rosazza <leslierosazza@gmail.com>
2024-10-23 18:04:52 -0500

    Actualizacion del form en linea

--- src/pages/FormularioEnLinea.jsx
+++ src/pages/FormularioEnLinea.jsx
@@ -7,17 +7,33 @@ import '../css/FormularioEnLinea.css';
 const {Text, Title} = Typography;
 
 const FormularioEnLinea = () => {
-    
-    const form = useRef();
+    const form = useRef(); 
     const [file, setFile] = useState(null);
-    const sendEmail = (e) => {
-        e.preventDefault();
+
+    const handleSubmit  = (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target); 
+
         const reader = new FileReader();
         reader.readAsDataURL(file);
-          emailjs
-          .sendForm('service_fcu1z92', 'template_7fa7p0d', form.current, {
-            publicKey: 'zkaVYICRq4xmLNbPT',
-          })
+
+        reader.onload = async () => {
+
+            const serviceId = "service_fcu1z92";
+            const templateId = "template_7fa7p0d";
+            const userId = "zkaVYICRq4xmLNbPT"; 
+
+            const emailParams = {
+                from_name: formData.get('from_name'),
+                subject: formData.get('subject'),
+                message: formData.get('message'),
+                phone: formData.get('phone'),
+                user_email: formData.get('user_email'),
+                file: reader.result,
+            };
+
+            emailjs.send(serviceId, templateId, emailParams, userId)
           .then(
             () => {
               console.log('Formulario en Línea enviado con éxito!');
@@ -28,6 +44,8 @@ const FormularioEnLinea = () => {
               console.error('Error al enviar el Formulario en Línea:', error.text);
             },
           );
+        }
+          
       };
 
     return (
@@ -41,7 +59,7 @@ const FormularioEnLinea = () => {
                 <Text className='textoF'>
                     Para los clientes que deseen rellenar el formulario y enviar documentos de manera electrónica, por favor complete el siguiente formulario:
                 </Text>
-                <form ref={form} onSubmit={sendEmail} className='field'>
+                <form ref={form} onSubmit={handleSubmit} className='field'>
                     <div className="field">
                         <label htmlFor="">Nombres y Apellidos</label>
                         <input type='text' name='from_name' required/>
@@ -67,7 +85,7 @@ const FormularioEnLinea = () => {
                         <textarea name='message' rows={15} required />
                     </div>
                     <div className='field'>
-                        <button className='botonF' type="primary">Enviar</button>
+                        <button className='botonF' type="submit">Enviar</button>
                     </div>
                 </form>
             </Row>
